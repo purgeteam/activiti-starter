1238708136d924b5d837995a1d47f3917e50e883
Purgeyao <zhaoy@deepblueai.com>
2019-10-11 10:35:47 +0800

    upd

---
+++ activiti7-cloud-springboot-demo/src/main/java/com/purgeteam/activiti7/cloud/demo/Activiti7CloudApplication.java
@@ -0,0 +1,17 @@
+package com.purgeteam.activiti7.cloud.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author purgeyao
+ * @since 1.0
+ */
+@SpringBootApplication
+public class Activiti7CloudApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Activiti7CloudApplication.class, args);
+  }
+
+}
